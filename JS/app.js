e96cee4c21b448e1f9437f08c4912e24b3c7c1c5
dnor-dev <anorue4rss@hotmail.com>
2020-07-16 12:13:26 -0700

    UI ready

---
+++ JS/app.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.customer', {
+    delay:100,
+    duration:900,
+    reset:true,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.features', {
+    delay:100,
+    duration:900,
+    reset:true,
+    origin:'top',
+    distance:'50px'
+});
+
+sr.reveal('.top', {
+    delay:100,
+    duration:300,
+    reset:true,
+    origin:'top',
+    distance:'50px'
+});
+
+sr.reveal('.right', {
+    delay:100,
+    duration:900,
+    reset:true,
+    origin:'right',
+    distance:'300px'
+});
+
+sr.reveal('.bottom', {
+    delay:100,
+    duration:900,
+    reset:true,
+    origin:'bottom',
+    distance:'350px'
+});
