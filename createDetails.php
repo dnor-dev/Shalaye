33b1365f0003d625da164372375e3d1a75f77a42
dnor-dev <anorue4rss@hotmail.com>
2020-07-17 06:09:22 -0700

    email changes

--- createDetails.php
+++ createDetails.php
@@ -5,9 +5,10 @@ $message = '';
 
 if (isset ($_POST['submit'])){
     $email = $_POST['email'];
+    $email = mysqli_real_escape_string($conn, $email);
 
 
-    $query = "INSERT INTO capture (email) VALUES ('$email')";
+    $query = "INSERT INTO capture (email) VALUE ('$email')";
     $result = mysqli_query($conn, $query);
 
     if ($result){
